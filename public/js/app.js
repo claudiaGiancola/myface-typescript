39e02e1b3840b401b6c16c256304ab82e0bd36cd
ruksan_emirali <ruksanemirali@gmail.com>
2024-07-18 14:32:09 +0100

    Posts page design implemented with animation added to nav bar
    Co-authored-by: Claudia Giancola <claudiaGiancola@users.noreply.github.com>

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,39 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Add your JavaScript here.
+  
+  const changeColourButton = document.getElementById("change-colour");
+  const posts = document.querySelectorAll(".posts");
+  const hamburgerButton = document.querySelector(".hamburger-button");
+  const navbar = document.querySelector(".navbar");
+  const postListBody = document.querySelector("#post-list");
+
+  changeColourButton.addEventListener("click", function () {
+    posts.forEach((post) => post.style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`);
+  })
+
+hamburgerButton.addEventListener("click", function(){
+  navbar.classList.toggle("hidden");
+})
+
+postListBody.addEventListener("click", function() {
+  navbar.classList.add("hidden");
+})
+
+const coverPic = document.querySelector(".cover-pic");
+
+coverPic.style.classList.add("hidden");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
+
